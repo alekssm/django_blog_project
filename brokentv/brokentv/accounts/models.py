df3670d2c55c479a51e63ab0d853d6baa4fcf36a
Sim <113828515+alekssm@users.noreply.github.com>
2022-10-31 13:21:51 +0200

    creating custom auth user

--- brokentv/brokentv/accounts/models.py
+++ brokentv/brokentv/accounts/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
-# Create your models here.
+from brokentv.accounts.managers import BrokenTvUsersManager
+
+
+class BrokenTvUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        unique=True,
+    )
+
+    is_staff = models.BooleanField(
+        default=False,
+    )
+
+    date_joined = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    USERNAME_FIELD = 'email'
+
+    objects = BrokenTvUsersManager()
--- brokentv/brokentv/settings.py
+++ brokentv/brokentv/settings.py
@@ -76,13 +76,23 @@ WSGI_APPLICATION = 'brokentv.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'brokentv_db',
+        'USER': 'postgres',
+        'PASSWORD': '1123QwER',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -124,3 +134,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.BrokenTvUser'
